959789df05bd9c0bd3ae84553549aa51070640e0
yellow-man <yellow-man@users.noreply.github.com>
2017-03-15 00:34:43 +0900

    [タスク]株価情報が欲しい。 · Issue #2 · yellow-man/sena-batch ・微調整。

--- src/app/yokohama/yellow_man/sena/core/components/db/StocksComponent.java
+++ src/app/yokohama/yellow_man/sena/core/components/db/StocksComponent.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import com.avaje.ebean.Ebean;
+import com.avaje.ebean.ExpressionList;
 
 import yokohama.yellow_man.sena.core.components.db.dao.StocksDao;
 import yokohama.yellow_man.sena.core.models.Stocks;
@@ -13,6 +14,7 @@ import yokohama.yellow_man.sena.core.models.Stocks;
  *
  * @author yellow-man
  * @since 1.0.0
+ * @version 1.2.1
  * @see yokohama.yellow_man.sena.core.components.db.dao.StocksDao
  */
 public class StocksComponent extends StocksDao {
@@ -53,6 +55,34 @@ public class StocksComponent extends StocksDao {
 					.eq("delete_flg", false)
 					.orderBy("id ASC")
 					.findList();
+
+		return retList;
+	}
+
+	/**
+	 * 未削除の{@code minStockCode}以上、{@code maxStockCode}以下の銘柄（stocks）情報一覧を返す。
+	 * <p>また、{@code minStockCode}、{@code maxStockCode}の指定がない場合全件返す。
+	 *
+	 * @param minStockCode 取得対象最小銘柄コード
+	 * @param maxStockCode 取得対象最大銘柄コード
+	 * @return 未削除の銘柄（stocks）情報一覧
+	 * @since 1.2.1
+	 */
+	public static List<Stocks> getStocksList(Integer minStockCode, Integer maxStockCode) {
+
+		ExpressionList<Stocks> expressionList =
+				Ebean.find(Stocks.class)
+					.where()
+					.eq("delete_flg", false);
+
+		if (minStockCode != null) {
+			expressionList.ge("stock_code", minStockCode);
+		}
+		if (maxStockCode != null) {
+			expressionList.le("stock_code", maxStockCode);
+		}
+
+		List<Stocks> retList = expressionList.orderBy("id ASC").findList();
 
 		return retList;
 	}
